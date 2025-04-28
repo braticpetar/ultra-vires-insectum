2edaa59ffaa3f3fcb6797f9997c4fb23be247ec7
braticpetar <braticpetar44@proton.me>
2025-04-28 18:49:46 +0200

    Optimized number of animations

--- characters/school-girl/animations.lua
+++ characters/school-girl/animations.lua
@@ -1,15 +1,11 @@
 local Animation = require("libs.animation")
 
 local images = {
-  _idle_I = love.graphics.newImage("assets/school-girl/idle_I.png"),
-  _idle_II = love.graphics.newImage("assets/school-girl/idle_II.png"),
-  _idle_III = love.graphics.newImage("assets/school-girl/idle_III.png"),
-  _idle_IV = love.graphics.newImage("assets/school-girl/idle_IV.png"),
+  idle_up = love.graphics.newImage("assets/school-girl/idle_I.png"),
+  idle_down = love.graphics.newImage("assets/school-girl/idle_IV.png"),
 
-  _running_I = love.graphics.newImage("assets/school-girl/walk_I.png"),
-  _running_II = love.graphics.newImage("assets/school-girl/walk_II.png"),
-  _running_III = love.graphics.newImage("assets/school-girl/walk_III.png"),
-  _running_IV = love.graphics.newImage("assets/school-girl/walk_IV.png"),
+  running_up = love.graphics.newImage("assets/school-girl/walk_I.png"),
+  running_down = love.graphics.newImage("assets/school-girl/walk_IV.png"),
 }
 
 -- Animation.new(image, frames_number, fps, flipped, scale)
@@ -18,13 +14,13 @@ local fps = 10
 local scale = 3
 
 return {
-  idle_I = Animation.new(images._idle_I, frames_number, fps, false, scale),
-  idle_II = Animation.new(images._idle_II, frames_number, fps, false, scale),
-  idle_III = Animation.new(images._idle_III, frames_number, fps, false, scale),
-  idle_IV = Animation.new(images._idle_IV, frames_number, fps, false, scale),
+  idle_I = Animation.new(images.idle_up, frames_number, fps, false, scale),
+  idle_II = Animation.new(images.idle_up, frames_number, fps, true, scale),
+  idle_III = Animation.new(images.idle_down, frames_number, fps, true, scale),
+  idle_IV = Animation.new(images.idle_down, frames_number, fps, false, scale),
 
-  running_I = Animation.new(images._running_I, frames_number, fps, false, scale),
-  running_II = Animation.new(images._running_II, frames_number, fps, false, scale),
-  running_III = Animation.new(images._running_III, frames_number, fps, false, scale),
-  running_IV = Animation.new(images._running_IV, frames_number, fps, false, scale),
+  running_I = Animation.new(images.running_up, frames_number, fps, false, scale),
+  running_II = Animation.new(images.running_up, frames_number, fps, true, scale),
+  running_III = Animation.new(images.running_down, frames_number, fps, true, scale),
+  running_IV = Animation.new(images.running_down, frames_number, fps, false, scale),
 }
